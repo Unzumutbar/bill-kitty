ef6dac38ff6ec05343b29206d7d1057bc893deda
unknown <nilsbuergel@gmail.com>
2020-11-14 02:12:26 +0100

    Ionic Package hinzugefügt

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,14 +6,14 @@ export const environment = {
   production: false,
 
   firebase: {
-    apiKey: "AIzaSyDzDd54Yezc_t-_7FEINALoQcTgmiZP634",
-    authDomain: "billkitten-nimo.firebaseapp.com",
-    databaseURL: "https://billkitten-nimo.firebaseio.com",
-    projectId: "billkitten-nimo",
-    storageBucket: "billkitten-nimo.appspot.com",
-    messagingSenderId: "569801489857",
-    appId: "1:569801489857:web:446398b0315a03554c4340",
-    measurementId: "G-3XK2YJT623"
+    apiKey: 'AIzaSyDzDd54Yezc_t-_7FEINALoQcTgmiZP634',
+    authDomain: 'billkitten-nimo.firebaseapp.com',
+    databaseURL: 'https://billkitten-nimo.firebaseio.com',
+    projectId: 'billkitten-nimo',
+    storageBucket: 'billkitten-nimo.appspot.com',
+    messagingSenderId: '569801489857',
+    appId: '1:569801489857:web:446398b0315a03554c4340',
+    measurementId: 'G-3XK2YJT623'
   }
 };
 
